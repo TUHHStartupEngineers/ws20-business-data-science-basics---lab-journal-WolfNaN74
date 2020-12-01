017c1d95f4b0e1d50a797c3583890a76d944e1da
WolfNaN74 <74736613+WolfNaN74@users.noreply.github.com>
2020-12-01 11:51:06 +0100

    Upload challenge Chapter 2

--- build_website.R
+++ build_website.R
@@ -3,7 +3,7 @@
 ##########################################################################
 ##### DON'T PUSH THIS SECTION TO GITHUB  - REMOVE THIS AFTER RUNNING #####
 ##########################################################################
-# Sys.setenv(LAB_KEY = "Your-token")
+#Sys.setenv(LAB_KEY = "MY TOKEN")
 
 # Step 1: Build Website ---------------------------------------------------
 rmarkdown::render_site(encoding = 'UTF-8')
